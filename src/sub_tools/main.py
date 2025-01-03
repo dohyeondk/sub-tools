a0aed871b49c1262f1679c4342556ee09b6101e9
Dohyeon Kim <me@dohyeon.com>
2025-01-02 19:01:57 -0600

    Download the video for easy validation

--- src/sub_tools/main.py
+++ src/sub_tools/main.py
@@ -1,7 +1,7 @@
 import signal
 
 from .arguments.parser import build_parser, parse_args
-from .media.converter import hls_to_media, media_to_signature
+from .media.converter import hls_to_media, media_to_signature, video_to_audio
 from .media.segmenter import segment_audio
 from .subtitles.combiner import combine_subtitles
 from .system.directory import change_directory
@@ -15,7 +15,8 @@ def main():
 
         if parsed.hls_url:
             change_directory(parsed.output_path)
-            hls_to_media(parsed.hls_url, parsed.audio_file, True, parsed.overwrite)
+            hls_to_media(parsed.hls_url, parsed.video_file, False, parsed.overwrite)
+            video_to_audio(parsed.video_file, parsed.audio_file, parsed.overwrite)
             media_to_signature(parsed.audio_file, parsed.shazam_signature_file, parsed.overwrite)
             segment_audio(parsed.audio_file, parsed.audio_segment_prefix, parsed.audio_segment_format, parsed.audio_segment_length)
             transcribe(parsed)
