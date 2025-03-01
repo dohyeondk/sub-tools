491eca71341c4cb3bd5459ff42be06e7da318866
Dohyeon Kim <me@dohyeon.com>
2025-03-01 11:38:39 -0600

    resolve #9

--- src/sub_tools/main.py
+++ src/sub_tools/main.py
@@ -12,29 +12,32 @@ def main():
         parsed = parse_args(parser)
 
         change_directory(parsed.output_path)
+
         if "video" in parsed.tasks:
-            if parsed.hls_url:
-                hls_to_media(parsed.hls_url, parsed.video_file, False, parsed.overwrite)
-            else:
-                print("Error: No HLS URL provided")
-                parsed.func()
-                exit(1)
+            if not parsed.hls_url:
+                raise Exception("No HLS URL provided")
+            hls_to_media(parsed.hls_url, parsed.video_file, False, parsed.overwrite)
+
         if "audio" in parsed.tasks:
             video_to_audio(parsed.video_file, parsed.audio_file, parsed.overwrite)
+
         if "signature" in parsed.tasks:
             media_to_signature(parsed.audio_file, parsed.signature_file, parsed.overwrite)
+
         if "segment" in parsed.tasks:
             segment_audio(parsed.audio_file, parsed.audio_segment_prefix, parsed.audio_segment_format, parsed.audio_segment_length, parsed.overwrite)
+
         if "transcribe" in parsed.tasks:
-            if parsed.gemini_api_key and parsed.gemini_api_key.strip():
-                transcribe(parsed)
-            else:
-                print("Error: No Gemini API Key provided")
-                parsed.func()
-                exit(1)
+            if not (parsed.gemini_api_key and parsed.gemini_api_key.strip()):
+                raise Exception("No Gemini API Key provided")
+            transcribe(parsed)
+
         if "combine" in parsed.tasks:
             combine_subtitles(parsed.languages, parsed.audio_segment_prefix, parsed.audio_segment_format)
+
         print("Done!")
 
     except Exception as e:
         print(f"Error: {str(e)}")
+        parsed.func()
+        exit(1)
