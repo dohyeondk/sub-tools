8222ee38d2b865bd0f293a69403c6dfa550fa0ee
Dohyeon Kim <me@dohyeon.com>
2025-11-22 13:31:24 -0600

    chore: Remove scripts directory and update .gitignore
    - Delete scripts/get_hls_url.sh
    - Add scripts/ to .gitignore to exclude future script files

--- scripts/get_hls_url.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-HLS_URL=$(curl "https://api.life.church/v2/messages?key=ecafba80c4f683b3b0e70d9c8de71dae&include=series&quantity=1&type=sermon" | jq '.[0].formats[] | select(.name == "download_video") | .url' -r)
-
-echo "$HLS_URL"
-
-# uv run sub-tools -i "$HLS_URL" --overwrite
